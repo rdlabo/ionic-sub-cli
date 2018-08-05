e4fe17cc77c70829fd84874051beb898b30cc610
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-05 21:47:20 +0900

    feat(color): log color set.

--- src/commands/set.ts
+++ src/commands/set.ts
@@ -2,6 +2,7 @@ import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
 const fs = require('fs')
 const exec = require('child_process').exec
+const chalk = require('chalk')
 
 export default class Set extends Command {
   static description = 'auto set config. input arg `lint` or `prettier`' +
@@ -25,18 +26,18 @@ export default class Set extends Command {
     } else if (args.package === 'prettier') {
       this.prettier().catch()
     } else {
-      this.log('Please input set package. lint or prettier.')
+      this.log(chalk.red('Please input set package. lint or prettier.'))
     }
   }
   async lint() {
     // npm install codelyzer
-    cli.action.start('npm install codelyzer --save-dev')
+    cli.action.start('> ' + chalk.green('npm install codelyzer --save-dev'))
     await new Promise(resolve => {
       exec('npm install codelyzer --save-dev', (error: any) => {
         if (error) {
-          this.log('Sorry. failed npm install. You should exec npm install codelyzer --save-dev')
+          this.log(chalk.red('Sorry. failed npm install. You should exec npm install codelyzer --save-dev'))
         } else {
-          this.log('Complete install codelyzer --save-dev')
+          this.log('[' + chalk.green('OK') + '] ' + 'Complete install codelyzer --save-dev')
         }
         cli.action.stop()
         resolve()
@@ -46,20 +47,20 @@ export default class Set extends Command {
     // change tsLintRule
     fs.writeFile('./tslint.json', tsLintRule, (error: any) => {
       if (error) {
-        this.log('Sorry! ionic-ad did not re-write tslint-rule.')
+        this.log(chalk.red('Sorry! ionic-ad did not re-write tslint-rule.'))
       } else {
-        this.log('Complete re-write tslint-rule.')
+        this.log('[' + chalk.green('OK') + '] ' + 'Complete re-write tslint-rule.')
       }
     })
   }
   async prettier() {
-    cli.action.start('npm install prettier @kaizenplatform/prettier-config pre-commit --save-dev')
+    cli.action.start('> ' + chalk.green('npm install prettier @kaizenplatform/prettier-config pre-commit --save-dev'))
     await new Promise(resolve => {
       exec('npm install prettier @kaizenplatform/prettier-config pre-commit --save-dev', (error: any) => {
         if (error) {
-          this.log('Sorry. failed npm install. You should exec npm install @kaizenplatform/prettier-config --save-dev')
+          this.log(chalk.red('Sorry. failed npm install. You should exec npm install @kaizenplatform/prettier-config --save-dev'))
         } else {
-          this.log('Complete install prettier @kaizenplatform/prettier-config pre-commit --save-dev')
+          this.log('[' + chalk.green('OK') + '] ' + 'Complete install prettier @kaizenplatform/prettier-config pre-commit --save-dev')
         }
         cli.action.stop()
         resolve()
@@ -68,21 +69,21 @@ export default class Set extends Command {
 
     fs.writeFile('./prettier.config.js', 'module.exports =require(\'@kaizenplatform/prettier-config\');', (error: any) => {
       if (error) {
-        this.log('Sorry! ionic-ad did not write prettier.config.js.')
+        this.log(chalk.red('Sorry! ionic-ad did not write prettier.config.js.'))
       } else {
-        this.log('create prettier.config.js')
+        this.log('[' + chalk.green('OK') + '] ' + 'create prettier.config.js')
       }
     })
 
     fs.readFile('./package.json', 'utf8', (error: any, data: string) => {
       if (error) {
-        this.log('Sorry! ionic-ad did not read package.json.')
+        this.log(chalk.red('Sorry! ionic-ad did not read package.json.'))
         return
       }
 
       const executed = data.match(/\"pre-commit: {\"/g)
       if (executed) {
-        this.log('package.json have \'"pre-commit: {\' line. this command do not overwrite.')
+        this.log(chalk.red('package.json have \'"pre-commit: {\' line. this command do not overwrite.'))
         return
       }
 
@@ -93,9 +94,9 @@ export default class Set extends Command {
 
       fs.writeFile('./package.json', data, (error: any) => {
         if (error) {
-          this.log('Sorry! ionic-ad did not re-write package.json/')
+          this.log(chalk.red('Sorry! ionic-ad did not re-write package.json/'))
         } else {
-          this.log('write prettier and pre-commit in package.json')
+          this.log('[' + chalk.green('OK') + '] ' + 'write prettier and pre-commit in package.json')
         }
         return
       })
