6c477862d1961327d2eea58237ea5ed408b93b0b
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-05 17:54:05 +0900

    WIP: fix(hook): ignore ionic project

--- src/hooks/prerun/check_ionic.ts
+++ src/hooks/prerun/check_ionic.ts
@@ -1,15 +1,16 @@
 import {Hook} from '@oclif/config'
 
-const hook: Hook<'prerun'> = async function (opts) {
-  const fs = require('fs')
-  const filePath = './ionic.config.json'
-
-  fs.access(filePath, fs.constants.R_OK | fs.constants.W_OK, (error: any) => {
-    if (error) {
-      this.log('Sorry! ionic-ad can only be run in an Ionic project directory.')
-      return
-    }
-  })
+// todo: check ionic platform.
+const hook: Hook<'prerun'> = async function () {
+  // const fs = require('fs')
+  // const filePath = './ionic.config.json'
+  //
+  // fs.access(filePath, fs.constants.R_OK | fs.constants.W_OK, (error: any) => {
+  //   if (error) {
+  //     this.warn('Sorry! ionic-ad can only be run in an Ionic project directory.')
+  //     this.exit()
+  //   }
+  // })
 }
 
 export default hook
