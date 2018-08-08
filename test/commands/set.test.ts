be14b7bc504ea8d2c514694ff2df5dd7511e9fa7
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-08 09:08:54 +0900

    test(set/ionic-angular): add prettier and alias test

--- test/commands/set.test.ts
+++ test/commands/set.test.ts
@@ -2,7 +2,7 @@ import {expect, test} from '@oclif/test'
 const chalk = require('chalk')
 const fs = require('fs')
 const mock = require('mock-fs')
-import {Lint} from '../../src/libraries'
+import {Alias, Lint, Prettier} from '../../src/libraries'
 
 describe('Failed set lint', () => {
   test
@@ -17,10 +17,11 @@ describe('Testing libraries/set', () => {
     mock({
       'ionic.config.json': fs.readFileSync('./mock/ionic-angular/ionic.config.json', 'utf-8'),
       'package.json': fs.readFileSync('./mock/ionic-angular/package.json', 'utf-8'),
+      'tsconfig.json': fs.readFileSync('./mock/ionic-angular/tsconfig.json', 'utf-8'),
       src: {
         template: {
           'tslint.json': fs.readFileSync('./src/template/tslint.json', 'utf-8'),
-          'ionic-angular': {
+          ionic_angular: {
             'environment.dev.ts.txt': fs.readFileSync('./src/template/ionic_angular/environment.dev.ts.txt', 'utf-8'),
             'environment.prod.ts.txt': fs.readFileSync('./src/template/ionic_angular/environment.prod.ts.txt', 'utf-8'),
             'webpack.config.txt': fs.readFileSync('./src/template/ionic_angular/webpack.config.txt', 'utf-8'),
@@ -36,8 +37,24 @@ describe('Testing libraries/set', () => {
   })
 
   it('Testing libraries/set/lint', async () => {
-    const lint = new Lint('ionic-angular', {dry: true})
-    expect(await lint.addLint()).to.include(chalk.green('OK'))
-    expect('lint.installPackage()')
+    const Lib = new Lint('ionic-angular', {})
+    expect('installPackage()')
+    expect(await Lib.addLint()).to.include(chalk.green('OK'))
+  })
+
+  it('Testing libraries/set/prettier', async () => {
+    const Lib = new Prettier('ionic-angular', {})
+    expect('installPackage()')
+    expect(await Lib.addPrettierConfig()).to.include(chalk.green('OK'))
+    expect(await Lib.rewritePackageJson()).to.include(chalk.green('OK'))
+  })
+
+  it('Testing libraries/set/alias', async () => {
+    const Lib = new Alias('ionic-angular', {})
+    expect('installPackage()')
+    expect(await Lib.addWebpack()).to.include(chalk.green('OK'))
+    expect(await Lib.rewritePackageJson()).to.include(chalk.green('OK'))
+    expect(await Lib.rewriteTsconfigJson()).to.include(chalk.green('OK'))
+    expect(await Lib.addEnvironmentFile()).to.include(chalk.green('OK'))
   })
 })
