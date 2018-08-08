59f4936840cf4bad811c272244a6b1722856a330
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-08 15:10:16 +0900

    feat(helper): add version check function.

--- src/libraries/helper.ts
+++ src/libraries/helper.ts
@@ -1,9 +1,17 @@
+import {execSync} from 'child_process'
+const chalk = require('chalk')
 const fs = require('fs')
 
 export class Helper {
   public getIonicType(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      fs.readFile('./ionic.config.json', 'utf8', (error: any, data: string) => {
+    return new Promise(async (resolve, reject) => {
+      await this.checkNpmVersion('ionic', 'ionic').catch(error => {
+        process.stdout.write(error + '\n')
+      })
+      await this.checkNpmVersion('@rdlabo/ionic-sub', 'ionic-sub').catch(error => {
+        process.stdout.write(error + '\n')
+      })
+      await fs.readFile('./ionic.config.json', 'utf8', (error: any, data: string) => {
         if (error) {
           reject('Sorry! ionic-sub CLI can only be run in an Ionic project directory.')
           return
@@ -15,6 +23,20 @@ export class Helper {
         }
         resolve(config.type)
       })
+    })
+  }
+  public async checkNpmVersion(pkg: string, command: string) {
+    return new Promise(async (resolve, reject) => {
+      const v = await execSync('npm show ' + pkg + ' version').toString().match(/(\d|\.)+/)
+      const l = await execSync(command + ' -v').toString().match(/(\d|\.)+/)
+      if (l && l[0] && v && v[0]) {
+        if (v[0] > l[0]) {
+          const message = 'Local npm package `' + pkg + '` is old (Local ' + v[0] + ' > New ' + l[0] + '). Please exec `npm install ' + pkg + ' -g`'
+          reject(chalk.red('[Warning] ') + message)
+          return
+        }
+      }
+      resolve()
     })
   }
 }
